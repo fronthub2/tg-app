717311eca3a36778238049893adc01cc3b4a318b
Денис Андросов <you@example.com>
2025-02-23 23:37:36 +0300

    update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BacklogComponent } from "./components/backlog/backlog.component";
+import { NavMenuComponent } from "./components/nav-menu/nav-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavMenuComponent, BacklogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
