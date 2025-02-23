56395afb1104534e6fcac0180c1975e50ea24b2f
Денис Андросов <you@example.com>
2025-02-23 23:40:31 +0300

    update board, feedback,nav-menu,sidebar

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss'
 })
